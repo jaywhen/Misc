bc86945769c0382506706e2f55622f630a587568
Xiang Jaywhen <jaywhenxiang@foxmail.com>
2020-08-20 23:21:12 +0800

    moyu

--- form/app.py
+++ form/app.py
@@ -1,18 +1,22 @@
 import os
-from flask import Flask, render_template, url_for
-from forms import LoginForm
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import LoginForm, UploadForm
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
-
+app.config['UPLOAD_PATH']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/basic')
+@app.route('/basic', methods=['GET', 'POST'])
 def basic():
    form = LoginForm()
+   if form.validate_on_submit():
+      username = form.username.data
+      flash('Welcome home! %s !' % username)
+      return redirect(url_for('index'))
    return render_template('basic.html', form = form)
 
 @app.route('/html')
@@ -23,3 +27,11 @@ def html():
 def bootstrap():
     form = LoginForm()
     return render_template('bootstrap.html', form=form)
+
+@app.route('/test')
+def test():
+   return render_template('jaywhenbase.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+   form = UploadForm()
