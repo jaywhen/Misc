d549b4192de04cdac2422cad104264b45edb0273
Xiang Jaywhen <jaywhenxiang@foxmail.com>
2020-08-22 23:14:06 +0800

    # sleep

--- database/app.py
+++ database/app.py
@@ -1,13 +1,92 @@
 import os
+import click
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, flash, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
 
-app.config['SQLALCHEMY_DATABASE URI'] = \
-os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(app.root_path, 'data.db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(app.root_path, 'data.db'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+
+# forms
+class NewNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('save')
+
+class EditNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+class DeleteNoteForm(FlaskForm):
+    submit = SubmitField('Delete')
+
+@app.route('/')
+def index():
+    form = DeleteNoteForm()
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes, form=form)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_note():
+    form = NewNoteForm()
+    if form.validate_on_submit():
+       body = form.body.data
+       note = Note(body=body)
+       db.session.add(note)
+       db.session.commit()
+       flash('Your note is saved.')
+       return redirect(url_for('index'))
+    return render_template('new_note.html', form=form)
+    
+@app.route('/edit/<int:note_id>', methods=['GET', 'POST'])
+def edit_note(note_id):
+    form = EditNoteForm()
+    note = Note.query.get(note_id)
+    if form.validate_on_submit():
+        note.body = form.body.data
+        db.session.commit()
+        flash('Your note is updated.')
+        return redirect(url_for('index'))
+    form.body.data = note.body  # preset form input's value
+    return render_template('edit_note.html', form=form)
+
+@app.route('/delete/<int:note_id>', methods=['POST'])
+def delete_note(note_id):
+    form = DeleteNoteForm()
+    if form.validate_on_submit():
+        note = Note.query.get(note_id)
+        db.session.delete(note)
+        db.session.commit()
+        flash('Your note is deleted.')
+    else:
+        abort(400)
+    return redirect(url_for('index'))
+
+
+
+
+
+# 定义模型，对应数据库的一张表
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Note %r>' % self.body
+
+@app.cli.command()
+def initdb():
+    db.create_all()
+    click.echo('Initialized database.')
