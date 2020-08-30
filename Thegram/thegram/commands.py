efa5002128e66b5c9949442a891a3c8fa9344a2b
Xiang Jaywhen <jaywhenxiang@foxmail.com>
2020-08-30 15:30:02 +0800

    add more tables

--- Thegram/thegram/commands.py
+++ Thegram/thegram/commands.py
@@ -1,5 +1,11 @@
 import click
 from thegram import app, db
+from thegram.models import User, Image, Comment
+
+import random
+
+def get_image_url():
+    return 'http://images.nowcoder.com/head/' + str(random.randint(0,1000)) + 'm.png'
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop')
@@ -11,7 +17,30 @@ def initdb(drop):
         click.echo('Database has been droped')
     db.create_all()
     click.echo('Initialized database')
+    # add some user
+    for i in range(0, 3):
+        user = User(
+            username = 'User' + str(i),
+            password = 'a' + str(i)
+        )
+        db.session.add(user)
+
+        for j in range(0, 2):
+            image = Image(get_image_url(), i+1)
+            db.session.add(image)
+
+            for k in range(0, 2):
+                comment = Comment(
+                    content = 'This is a test comment' + str(k),
+                    image_id = 1 + 2*i + j,
+                    user_id = i + 1
+                ) 
+                db.session.add(comment)
+
+    db.session.commit()
+    click.echo('now added some users')
 
 @app.cli.command()
 def tcli():
+    """ for testing """
     print('hi')
--- Thegram/thegram/models.py
+++ Thegram/thegram/models.py
@@ -1,18 +1,55 @@
 from thegram import db
+
+from datetime import datetime
 import random
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     username = db.Column(db.String(20), unique = True)
     password = db.Column(db.String(16))
     head_url = db.Column(db.String(256))
+    images = db.relationship('Image')
 
     def __init__(self, username, password):
-        self._username = username
-        self._password = password
-        self._head_url = 'http://images.nowcoder.com/head/' + str(random.randint(0,1000)) + 'm.png'
+        self.username = username
+        self.password = password
+        self.head_url = 'http://images.nowcoder.com/head/' + str(random.randint(0,1000)) + 'm.png'
 
     def __repr__(self):
         return '<User %d %s>' %(self.id, self.username)
-        
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    url = db.Column(db.String(256)) 
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    created_data = db.Column(db.DateTime)
+    comments = db.relationship('Comment')
+
+    def __init__(self, url, user_id):
+        self.url = url
+        self.user_id = user_id
+        self.created_data = datetime.now()
+
+    def __repr__(self):
+        return '<Image %d %s>' %(self.id, self.url)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    content = db.Column(db.String(256))
+    image_id = db.Column(db.Integer, db.ForeignKey(Image.id))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    status = db.Column(db.Integer, default = 0) # 0正常 1被删
+    user = db.relationship('User')
+
+    def __init__(self, content, image_id, user_id):
+        self.content = content
+        self.image_id = image_id
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Comment %d %s>' %(self.id, self.content)
+
+
+
+
 
 
