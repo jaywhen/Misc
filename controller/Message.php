da3641631c9617e440fca81aafbf3e18c7b35013
Xiang Jaywhen <jaywhenxiang@foxmail.com>
2020-09-17 22:10:08 +0800

    add thumbup

--- controller/Message.php
+++ controller/Message.php
@@ -100,6 +100,90 @@ class Message {
 
     //     }
     // }
+    public function likes() {
+        if(!$_POST['message_id']) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '参数不足: message_id';
+            return json($return_data);
+        }
+        if(!$_POST['user_id']) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '参数不足: user_id';
+            return json($return_data);
+        }
+
+        $message = Db::table('message')->where('id', $_POST['message_id'])->find();
+
+        if(!$message) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '该条消息不存在';
+            return json($return_data);
+        }
+
+        $data = array();
+        $data['total_likes'] = $message['total_likes'] + 1;
+
+        $result = Db::table('message')->where('id', $_POST['message_id'])->save($data);
+        
+        if($result) {
+            $return_data = array();
+            $return_data['error_code'] = 0;
+            $return_data['msg'] = '点赞数据保存成功';
+            $return_data['data']['message_id'] = $_POST['message_id'];
+            $return_data['data']['total_likes'] = $data['total_likes'];
+
+            return json($return_data);
+        } else {
+            $return_data = array();
+            $return_data['error_code'] = 2;
+            $return_data['msg'] = '点赞数据保存失败';
+            return json($return_data);
+        }
+
+        
+
+
+    }
+
+    /*
+     * 删除指定树洞消息 
+     */
+    public function deleteMessage() {
+        if(!$_POST['message_id']) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '参数不足: message_id';
+            return json($return_data);
+        }
+        if(!$_POST['user_id']) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '参数不足: user_id';
+            return json($return_data);
+        }
+
+        $message = Db::table('message')->where('id', $_POST['message_id'])->find();
+        if($message) {
+            $result = Db::table('message')->where('id', $_POST['message_id'])->delete();
+            if($result) {
+                $return_data['error_code'] = 0;
+                $return_data['msg'] = '树洞消息删除成功';
+                return json($return_data);
+            } else {
+                $return_data['error_code'] = 2;
+                $return_data['msg'] = '树洞消息删除失败';
+                return json($return_data);
+            }
+        } else {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '待删除的树洞消息不存在';
+            return json($return_data);
+        }
+    }
 
 
 }
